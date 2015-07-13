1b312d71adaa0a15e58730eb72aab9244fa10f06
trifidtrifid <trifidtrifid@gmail.com>
2015-07-13 22:10:40 +0400

    add mcid flow class. add test xml with 100 packet from mcid flow

---
+++ elastic.iface/src/main/java/com/atalas/callaider/elastic/iface/McidFlow.java
@@ -0,0 +1,15 @@
+package com.atalas.callaider.elastic.iface;
+import java.util.Map;
+import java.util.List;;
+
+public class McidFlow {
+
+	public class GsmTransaction {
+		Map<String, Object> m_request;
+		Map<String, Object> m_response;
+	}
+
+	List<GsmTransaction> m_transactions;
+		
+	
+}

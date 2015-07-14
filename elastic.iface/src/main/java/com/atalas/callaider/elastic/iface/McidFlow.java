b854ea954662bd362584d2875a1cfd99f4bf2f9e
Alexey Cherviakov <ceo@vmesteonline.ru>
2015-07-14 19:27:20 +0300

    Save implemented

--- elastic.iface/src/main/java/com/atalas/callaider/elastic/iface/McidFlow.java
+++ elastic.iface/src/main/java/com/atalas/callaider/elastic/iface/McidFlow.java
@@ -4,7 +4,9 @@ import java.util.List;;
 
 public class McidFlow {
 
-	public class GsmTransaction {
+	public String id;
+	
+	public static class GsmTransaction {
 		Map<String, Object> m_request;
 		Map<String, Object> m_response;
 	}
